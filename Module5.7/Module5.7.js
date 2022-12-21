18fde0651f9c328e4c98f41709b6b0bec3d10ba7
6oMLight <105768904+6oMLight@users.noreply.github.com>
2022-12-21 12:27:07 +0500

    Add files via upload

---
+++ Module5.7/Module5.7.js
@@ -0,0 +1,10 @@
+//Задание 8
+
+let map = new Map();
+map.set("1", "string");
+map.set(1, "number");
+map.set(true, "boolean");
+
+map.forEach((value, key) => {
+    console.log(`<< Ключ - ${key}, значение - ${value} >>.`);
+});
